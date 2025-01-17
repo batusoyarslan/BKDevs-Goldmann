7f2e389bfbf23587fddd55b96ac8685d176f6217
Batuhan Soyarslan <batihansoyarslan50@gmail.com>
2025-01-17 18:27:53 +0300

    .

--- projects.js
+++ projects.js
@@ -6,7 +6,7 @@ function changeImage(thumbnail) {
     mainImage.src = thumbnail.src;
     mainImage.alt = thumbnail.alt;
     mainImage.classList.add("active");
-  }, 200);
+  }, 100);
   thumbnails.forEach((img) => img.classList.remove("active"));
   thumbnail.classList.add("active");
 }
@@ -21,7 +21,16 @@ function autoChangeImages() {
 }
 
 window.onload = () => {
-  changeImage(document.querySelector(".thumbnail.active"));
+  const activeThumbnail = document.querySelector(".thumbnail.active");
+  if (activeThumbnail) {
+    changeImage(activeThumbnail);
+  } else {
+    // Eğer aktif bir küçük resim yoksa, ilk küçük resmi seç
+    const firstThumbnail = document.querySelector(".thumbnail");
+    if (firstThumbnail) {
+      changeImage(firstThumbnail);
+    }
+  }
   autoChangeImages();
 };
 
